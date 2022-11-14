d3b9539192a2d64dfe770e69f96875ea4bf843e7
圆周率3014 <79385954+yzl3014@users.noreply.github.com>
2022-11-14 21:57:53 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,19 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '圆周率的监控面板',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u1770019-b20fc9eed73762d95f5220fe'
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 100,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -30,7 +27,7 @@ window.Config = {
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://yuanzj.top/'
     },
   ],
 };
